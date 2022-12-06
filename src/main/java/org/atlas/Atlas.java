66fc4ac1157d637cc9f4b94d98b130f3d916eb29
stolbovik <stolbov_svyatoslav_02@mail.ru>
2022-12-07 01:07:54 +0300

    add dependency

--- src/main/java/org/atlas/Atlas.java
+++ src/main/java/org/atlas/Atlas.java
@@ -4,10 +4,8 @@ package org.atlas;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class Atlas {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
