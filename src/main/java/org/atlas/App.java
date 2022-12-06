5a942c2b9565553227a79d6cad6a1fdc1debda69
stolbovik <stolbov_svyatoslav_02@mail.ru>
2022-12-07 00:35:31 +0300

    Initial commit

---
+++ src/main/java/org/atlas/App.java
@@ -0,0 +1,13 @@
+package org.atlas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
