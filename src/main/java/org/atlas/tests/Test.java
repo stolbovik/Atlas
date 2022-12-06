64f77d59ce7b0cc5450f9880c2b3bf6fe48dc58b
Alena Bushkina <aa-bushkina@mail.ru>
2022-12-07 02:33:04 +0300

    add BaseTest

---
+++ src/main/java/org/atlas/tests/Test.java
@@ -0,0 +1,12 @@
+package org.atlas.tests;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+
+public class Test extends BaseTest {
+  @DisplayName("Тест")
+  @Tag("development")
+  @org.junit.jupiter.api.Test
+  public void someTest() {
+  }
+}
