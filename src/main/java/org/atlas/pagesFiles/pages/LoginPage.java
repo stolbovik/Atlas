c189a97fcf82cf7c343bfa0b2181af09d722608b
Alena Bushkina <aa-bushkina@mail.ru>
2022-12-15 18:19:11 +0300

    make click to Bookmarks

--- src/main/java/org/atlas/pagesFiles/pages/LoginPage.java
+++ src/main/java/org/atlas/pagesFiles/pages/LoginPage.java
@@ -4,7 +4,7 @@ import io.qameta.atlas.webdriver.AtlasWebElement;
 import io.qameta.atlas.webdriver.WebPage;
 import io.qameta.atlas.webdriver.extension.FindBy;
 import io.qameta.atlas.webdriver.extension.Param;
-
+@SuppressWarnings("rawtypes")
 public interface LoginPage extends WebPage {
 
     String TO_INPUT = "//*[@name='{{ text }}']";
--- src/test/java/org/atlas/tests/TestEx.java
+++ src/test/java/org/atlas/tests/TestEx.java
@@ -2,6 +2,7 @@ package org.atlas.tests;
 
 import org.atlas.pagesFiles.Config;
 import org.atlas.pagesFiles.pages.LoginPage;
+import org.atlas.pagesFiles.pages.UserPage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -15,5 +16,8 @@ public class TestEx extends BaseTest {
     loginPage.input("st.email").sendKeys(Config.USERNAME);
     loginPage.input("st.password").sendKeys(Config.PASS);
     loginPage.button("login-form-actions").click();
+
+    UserPage userPage = site.onUserPage();
+    userPage.goToAdditionalSection("Закладки").click();
   }
 }
