78209190675971b0f99211327ee245249377a5b0
Alena Bushkina <aa-bushkina@mail.ru>
2022-12-23 16:58:40 +0300

    add bindings

--- src/test/java/org/atlas/Config/GuiceModule.java
+++ src/test/java/org/atlas/Config/GuiceModule.java
@@ -1,8 +1,7 @@
 package org.atlas.Config;
 
 import com.google.inject.AbstractModule;
-import org.atlas.Steps.BookmarksPageSteps;
-import org.atlas.Steps.LoginPageSteps;
+import org.atlas.Steps.*;
 import org.atlas.TestResources.UserInfo;
 import org.atlas.Tests.BaseTest;
 import org.jetbrains.annotations.NotNull;
@@ -22,6 +21,10 @@ public class GuiceModule extends AbstractModule {
     protected void configure() {
         bind(LoginPageSteps.class).toInstance(new LoginPageSteps(baseTest));
         bind(BookmarksPageSteps.class).toInstance(new BookmarksPageSteps(baseTest));
+        bind(VideoPageSteps.class).toInstance(new VideoPageSteps(baseTest));
+        bind(FeedPageSteps.class).toInstance(new FeedPageSteps(baseTest));
+        bind(UserPageSteps.class).toInstance(new UserPageSteps(baseTest, ID));
+        bind(GroupPageSteps.class).toInstance(new GroupPageSteps(baseTest));
         bind(UserInfo.class).toInstance(new UserInfo(NAME, ID));
     }
 }
